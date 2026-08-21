515ffa630fe6df230375fd3ea989197e68e8ad15
jose123xd21-alt <jose123xd21@gmail.com>
2026-08-21 12:16:17 +0200

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -8,3 +8,10 @@ export const firebaseConfig = {
   messagingSenderId: "216733813752",
   appId: "1:216733813752:web:e8da21bb3ae6004e4de40d"
 };
+
+// app.js importa esta constante para saber si debe iniciar Firebase.
+export const firebaseEnabled = Boolean(
+  firebaseConfig.apiKey &&
+  firebaseConfig.projectId &&
+  firebaseConfig.appId
+);
